6ca38d97d21105a4223e67b1752be582e7e30750
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2025-04-30 13:54:19 -0300

    refactor: alterado nome do diretório principal de chatbot_backend para chatbot

--- chatbot/chatbot/asgi.py
+++ chatbot/chatbot/asgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
 
 application = get_asgi_application()
